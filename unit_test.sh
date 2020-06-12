9d4661d0eb9abef115b6e27f1fd19a8597e2fab0
wilsonwnog2014 <wilsonwnog@126.com>
2020-06-12 18:46:33 +0800

    update env.bashrc, unit_test.sh

--- unit_test.sh
+++ unit_test.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 # -*- coding: utf-8 -*-
 
+tf="tf1.15"
+if [[ $# -gt 0 ]]; then
+    tf=$1
+fi
+
 # 单元测试
-source env.bashrc
+source env.bashrc $tf
 python3 my_deep_sort_app.py --feature_type 1 --display False
