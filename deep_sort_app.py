5a1ff081e533e0a28ded9f22896e2bce30930d0b
Rinat <51479797+Xrenya@users.noreply.github.com>
2020-07-22 12:02:13 +0900

    Update deep_sort_app.py
    Fixing typo

--- deep_sort_app.py
+++ deep_sort_app.py
@@ -171,7 +171,7 @@ def run(sequence_dir, detection_file, output_file, min_confidence,
             seq_info["detections"], frame_idx, min_detection_height)
         detections = [d for d in detections if d.confidence >= min_confidence]
 
-        # Run non-maxima suppression.
+        # Run non-maximum suppression.
         boxes = np.array([d.tlwh for d in detections])
         scores = np.array([d.confidence for d in detections])
         indices = preprocessing.non_max_suppression(
