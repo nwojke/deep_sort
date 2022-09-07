df4ce0f9804557add2a32d941a77acf3c436185f
navicester <navicester@qq.com>
2022-09-07 20:17:32 +0800

    Update generate_detections.py

--- tools/generate_detections.py
+++ tools/generate_detections.py
@@ -80,11 +80,15 @@ class ImageEncoder(object):
             graph_def = tf.GraphDef()
             graph_def.ParseFromString(file_handle.read())
         tf.import_graph_def(graph_def, name="net")
+        #self.input_var = tf.get_default_graph().get_tensor_by_name(
+        #    "net/%s:0" % input_name)
+        #self.output_var = tf.get_default_graph().get_tensor_by_name(
+        #    "net/%s:0" % output_name)
         self.input_var = tf.get_default_graph().get_tensor_by_name(
-            "net/%s:0" % input_name)
+            "%s:0" % input_name)
         self.output_var = tf.get_default_graph().get_tensor_by_name(
-            "net/%s:0" % output_name)
-
+            "%s:0" % output_name)
+        
         assert len(self.output_var.get_shape()) == 2
         assert len(self.input_var.get_shape()) == 4
         self.feature_dim = self.output_var.get_shape().as_list()[-1]
