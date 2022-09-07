ee62300d72d585e15d5d055dc8ffa00cfc6e755e
navicester <navicester@qq.com>
2022-09-07 20:12:30 +0800

    Update tracker.py

--- deep_sort/tracker.py
+++ deep_sort/tracker.py
@@ -132,8 +132,11 @@ class Tracker:
 
     def _initiate_track(self, detection):
         mean, covariance = self.kf.initiate(detection.to_xyah())
-        class_name = detection.get_class()
+        #class_name = detection.get_class()
         self.tracks.append(Track(
             mean, covariance, self._next_id, self.n_init, self.max_age,
-            detection.feature, class_name))
+            detection.feature))
+        #self.tracks.append(Track(
+        #    mean, covariance, self._next_id, self.n_init, self.max_age,
+        #    detection.feature, class_name))
         self._next_id += 1
