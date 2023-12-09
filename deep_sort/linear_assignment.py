b21f044766fe3d4e92e5150c6bde350238a5fcd3
Alexis Alva <alexis.alvajn@gmail.com>
2023-12-09 14:00:07 -0300

    -update: use linear_assignment from scipy instead of sklearn

--- deep_sort/linear_assignment.py
+++ deep_sort/linear_assignment.py
@@ -1,7 +1,7 @@
 # vim: expandtab:ts=4:sw=4
 from __future__ import absolute_import
 import numpy as np
-from sklearn.utils.linear_assignment_ import linear_assignment
+from scipy.optimize import linear_sum_assignment as linear_assignment
 from . import kalman_filter
 
 
